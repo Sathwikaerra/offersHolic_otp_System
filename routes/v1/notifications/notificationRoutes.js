c390183ac261e4900edda4e877ab81fb6fad08d5
Sathwikaerra <b192624@rgukt.ac.in>
2025-12-05 13:18:43 +0530

    notifications

--- routes/v1/notifications/notificationRoutes.js
+++ routes/v1/notifications/notificationRoutes.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import User from '../../../models/User.js'
 import { createNotification, deleteNotification, getAllNotifications, markNotificationAsRead } from '../../../controllers/notification/notificationController.js';
 import { authenticateAdmin, authenticateToken } from '../../../middlewares/auth/authMiddleware.js';
 
 const router = express.Router();
+
+router.post("/broadcast", async (req, res) => {
+  try {
+    const { title, body, data } = req.body;
+
+    const users = await User.find({ deviceToken: { $exists: true } });
+
+    for (const u of users) {
+      await sendPushNotification(u.deviceToken, title, body, data);
+    }
+
+    res.status(200).json({ message: "Broadcast sent!" });
+  } catch (error) {
+    console.error("Broadcast Notification Error:", error);
+    res.status(500).json({ message: "Failed to broadcast" });
+  }
+});
+
 
 
 
